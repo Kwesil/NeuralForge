65399f931fe90dbf3e7c1ee8c324d92114700ffb
Kwesil <nwabikejr@gmail.com>
2026-05-18 13:36:04 +0100

    added mock recommendation to the app

--- app/app.py
+++ app/app.py
@@ -3,9 +3,22 @@ from emotion.detect_emotion import detect_emotion
 
 st.title("NeuralForge")
 st.subheader("Emotion-Aware Restaurant Recommendation Agent")
+    
+recommendations = {
+    "happy": ["Sushi Spot", "BBQ House"],
+    "sad": ["Comfort Bites", "Warm Soup Cafe"],
+    "angry": ["Quiet Garden Reataurant"],
+    "neutral": ["City Grill"]
+}
 
 if st.button("Analyze Emotion"):
     emotion = detect_emotion()
 
     st.success(f"Detected Emotion: {emotion}")
+
+    recs = recommendations.get(emotion, ["City Grill"])
     
+    st.write("recommendation Restaurants:")
+
+    for r in recs:
+        st.write(f"- {r}")
