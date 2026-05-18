2a8355dbf7c6458ff684ee4a3b844fcef633f5df
Kwesil <nwabikejr@gmail.com>
2026-05-18 13:08:21 +0100

    create the emotion detector using deepface

---
+++ app/emotion/detect_emotion.py
@@ -0,0 +1,25 @@
+from deepface import DeepFace
+import cv2
+
+def detect_emotion():
+    cap = cv2.VideoCapture(0)
+
+    ret, frame = cap.read()
+
+    if not ret:
+        return None
+    
+    try:
+        result = DeepFace.analyze(
+            frame,
+            actions=['emotion'],
+            enforce_detection=False
+        )
+
+        emottion = result[0]['dominant_emotion']
+    except Exception:
+        emotion = "neutral"
+
+    cap.release()
+
+    return emotion
