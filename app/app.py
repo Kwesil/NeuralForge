b611adc3eee04e4dcc6f430dc90e3f514d1d97c2
Kwesil <nwabikejr@gmail.com>
2026-05-19 23:53:32 +0100

    linked the behaviour context from to the app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from emotion.detect_emotion import detect_emotion
+from utils.context_mapper import map_emotion_to_context
+
+st.set_page_config(page_title="NeuralForge", layout="centered")
 
 st.title("NeuralForge")
 st.subheader("Emotion-Aware Restaurant Recommendation Agent")
@@ -20,7 +23,14 @@ if st.button("Analyze Emotion"):
         dominant_emotion = result["dominant_emotion"]
         emotion_scores = result["emotion_scores"]
 
+        behavioral_context = map_emotion_to_context(emotion_scores)
+
     st.success(f"Detected Emotion: {dominant_emotion}")
 
-    st.write("Emotion Confidence Scores")
+    st.write("### Emotion Confidence Scores")
     st.json(emotion_scores)
+
+    st.write("### Behavioral Context")
+    st.info(behavioral_context["context"])
+
+    st.write(behavioral_context["description"])
