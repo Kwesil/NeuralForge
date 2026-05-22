60773d6311e27cf217ebc3593ad76a201af68664
Kwesil <nwabikejr@gmail.com>
2026-05-22 20:07:55 +0100

    created embeddings for words

---
+++ app/recommender/embedding_model.py
@@ -0,0 +1,20 @@
+from sentence_transformers import SentenceTransformer
+
+embedding_model = SentenceTransformer(
+    "all-MiniLM-l6-v2"
+)
+
+def create_combuned_features(df):
+    df["combined_features"] = (
+        df["categories"].fillna("") + " " + df["city"].fillna("")
+    )
+
+    return df
+
+def create_embeddings(df):
+    embeddings = embedding_model.encode(
+        df["combined_features"].tolist(),
+        show_progress_bar=True
+    )
+
+    return embeddings
