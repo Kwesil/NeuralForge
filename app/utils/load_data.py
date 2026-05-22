c8c06ec8ad0b67532bc364b7870bdb836a1380e8
Kwesil <nwabikejr@gmail.com>
2026-05-22 20:04:56 +0100

    fixed the spelling bug

--- app/utils/load_data.py
+++ app/utils/load_data.py
@@ -7,7 +7,7 @@ def load_restaurants(json_path):
     with open(json_path, "r", encoding="utf-8") as file:
 
         for line in file:
-            business = json.loa -ds(line)
+            business = json.loads(line)
 
             # Keep only restaurants
             categories = business.get("categories")
