79eabf3a4bb1a2f2130e5d858cdfdd8ce4e0340d
Kwesil <nwabikejr@gmail.com>
2026-05-23 13:51:52 +0100

    added a space between funsocial

--- app/utils/context_query.py
+++ app/utils/context_query.py
@@ -2,7 +2,7 @@ def context_to_query(context):
     queries = {
         "comfort_seeking": "quiet comforting ramen cafe warm atmosphere",
         "stress_relief": "healthy peaceful relaxing restaurant calm ambience",
-        "social_exploration": "trendy energetic funsocial dining esxperience",
+        "social_exploration": "trendy energetic fun social dining esxperience",
         "balanced": "popular highly rated restaurant"
     }
 
