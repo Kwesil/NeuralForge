a144915944f35d107ea5cf900c215b58c41434ab
Kwesil <nwabikejr@gmail.com>
2026-05-24 04:49:32 +0100

    switch from local data to cloud data

--- app/utils/load_data.py
+++ app/utils/load_data.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import json
+import gdown
+import os
 
 def load_restaurants(json_path):
+    if not os.path.exists(json_path):
+        os.makedirs("data", exist_ok=True)
+        gdown.download(
+            "https://drive.google.com/file/d/1NxI4SqK1qLXXh9_PXjLI7uzPm-O2LaOI/view?usp=drive_link",
+            json_path,
+            quiet=False
+        )
+    
     data = []
-
     with open(json_path, "r", encoding="utf-8") as file:
-
         for line in file:
             business = json.loads(line)
-
-            # Keep only restaurants
             categories = business.get("categories")
-
             if categories and "Restaurants" in categories:
                 data.append({
                     "name": business.get("name"),
@@ -21,6 +26,4 @@ def load_restaurants(json_path):
                     "categories": categories
                 })
 
-    df = pd.DataFrame(data)
-    
-    return df
+    return pd.DataFrame(data)
